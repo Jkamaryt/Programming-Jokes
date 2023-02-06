6149cdeac3252fddbee22228f4a82d53ede9618f
Jack Kamaryt <jkamaryt46@gmail.com>
2023-02-06 17:30:52 -0600

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Jack Kamaryt on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
