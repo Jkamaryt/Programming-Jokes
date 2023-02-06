27b251e09d9e7b495927d5680d443205442fbf8d
Jack Kamaryt <jkamaryt46@gmail.com>
2023-02-06 17:58:07 -0600

    Got API Key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -8,19 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var jokes = [Joke]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(jokes){ joke in
+                NavigationLink(destination: Text(joke.punchline).padding())
+                {
+                    Text(joke.setup)
+                }
+            }
+            .navigationTitle("Programming Jokes")
         }
-        .padding()
+        .onAppear(){ getJokes()}
     }
+    func getJokes() {
+            let apiKey = "?rapidapi-key=b17bc0642fmshebd29ff68c7a379p10ba43jsndde2f61e9523"
+        }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Joke: Identifiable {
+    let id = UUID()
+    var setup = ""
+    var punchline = ""
 }
